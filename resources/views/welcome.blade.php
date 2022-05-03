526e4c10d34b2038b395cdb5e39b92162efeb4a1
Phạm Hiền <thecong1996@gmail.com>
2022-05-03 21:40:19 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -117,7 +117,7 @@
                             </svg>
 
                             <a href="https://github.com/sponsors/taylorotwell" class="ml-1 underline">
-                                Sponsor hello
+                                Sponsor hello 1
                             </a>
                         </div>
                     </div>
